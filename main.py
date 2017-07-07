d6da61565dde0b17dba7e35dd9b08ad2db221dae
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-07 12:47:27 +0530

    get_own_post() added main.py updated

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #main file
+from self_info import self_info
+from get_own_post import  get_own_post
+
+#fetching own information
+self_info()
+
+#fetching our own recent data
+result = get_own_post()
+print result
