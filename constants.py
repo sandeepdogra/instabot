22c92b0ea1a7666c336114efa4ccce9d2e5e24e5
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-07 11:37:10 +0530

    main.py and constants.py are added

---
+++ constants.py
@@ -0,0 +1,6 @@
+#all global variables are here
+
+#instagram api access token
+APP_ACCESS_TOKEN = '804302568.f825c9e.71d2e1af5a4b45f8b0cdfed1988ae40f'
+#instagram api base url
+BASE_URL = 'https://api.instagram.com/v1/'
