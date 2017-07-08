f86caab5ade9cce64ab8df6405593c960a02f077
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-08 22:37:12 +0530

    main file is added

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 #main file
 from self_info import self_info
-from get_own_post import  get_own_post
+from get_own_post import get_own_post
 
-
-#fetching own information
 self_info()
-
-#fetching our own recent data
+#fetching own information
 get_own_post()
-
+#getting own post
