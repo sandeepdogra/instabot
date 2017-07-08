e88a793dc0fc45e9cab2520124b869e55c7109a0
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-08 22:29:24 +0530

    comments_a_post file is added

--- comments_a_post.py
+++ comments_a_post.py
@@ -1,19 +1,18 @@
 import requests
 from constants import BASE_URL, APP_ACCESS_TOKEN
 from get_user_post import get_user_post
-username = "kirangarg95"
+username = "vineet_chauhan9"
+
 def post_a_comment(insta_username):
-    post_id = get_user_post(insta_username)
+    media_id = get_user_post(insta_username)
     # print media_id
-    request_url = (BASE_URL + ''
-                            'media/%s/comments') % (post_id)
     message = raw_input("Enter your Message: ")
     payload = {"access_token": APP_ACCESS_TOKEN, 'text':message}
-    print 'POST request url : %s' % (request_url)
+    request_url = (BASE_URL + 'media/%s/comments') % (media_id)
     post_a_comment = requests.post(request_url, payload).json()
-
+    print post_a_comment
     if post_a_comment['meta']['code'] == 200:
         print "Post comment Successfully"
     else:
         print "Unable to comment on post"
-post_a_comment(username)
+comment_user_post(username)
