f88e82aac8aaa5250470fd97e71e0bae027888b0
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-08 22:35:58 +0530

    like_user_post file is added

--- like_user_post.py
+++ like_user_post.py
@@ -1,14 +1,15 @@
 import requests
 from constants import BASE_URL, APP_ACCESS_TOKEN
+#from get_user_post import get_user_post
 from get_user_post import get_user_post
-username = "kirangarg95"
+username = "vineet_chauhan9"
 def like_a_post(insta_username):
-    post_id = get_user_post(insta_username)
-    # print media_id
-    request_url = (BASE_URL + ''
-                            'media/%s/likes') % (post_id)
+    # fuction is created for like post of user
+
+    media_id = get_user_post(insta_username)
+    print media_id
+    request_url = (BASE_URL + 'media/%s/likes') % (media_id)
     payload = {"access_token": APP_ACCESS_TOKEN}
-    print 'POST request url : %s' % (request_url)
     post_a_like = requests.post(request_url, payload).json()
 
     if post_a_like['meta']['code'] == 200:
